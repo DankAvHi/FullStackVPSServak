2e65e61bb5f2bdbbf725fbf76a6d3e636ea69c57
Даниил Дуров <81360775+DankAvHi@users.noreply.github.com>
2022-04-15 11:31:05 +0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,7 @@ const path = require("path");
 
 const cors = require("cors");
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8001;
 
 const app = express();
 app.use(express.static(__dirname + "/client/build"));
